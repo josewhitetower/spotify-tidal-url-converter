572554a8e14bad7f4c2abcbff2225e3082de033b
Jose Torreblanca <josewhitetower@gmail.com>
2019-11-16 00:19:36 +0100

    added vue, tailwindcss

---
+++ client/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="h-screen bg-gray-100">
+    <h1
+      class="text-xl md:text-3xl lg:text-3xl font-sans font-bold text-center pt-4"
+    >Spotify-Tidal URL Converter</h1>
+    <div>
+      <form class="text-center mt-8" @submit.prevent="submit">
+        <input type="text" placeholder="Enter URL..." required class="h-8 px-2" v-model="url" />
+        <button>Send</button>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    url: ""
+  }),
+  methods: {
+    submit() {
+      alert(this.url);
+      this.url = "";
+    }
+  }
+};
+</script>
+
+<style>
+</style>

7e4dd209221703bae4af2b286f3f8c61d9eef1a6
Jose Torreblanca <josewhitetower@gmail.com>
2019-11-16 16:10:18 +0100

    added TracksList component

--- client/src/App.vue
+++ client/src/App.vue
@@ -5,15 +5,7 @@
     >Spotify-Tidal URL Converter</h1>
     <div class="text-center">
       <SearchForm @submit="handleSubmit" />
-      <div
-        v-if="isLoading || tidalTracks.length > 0"
-        class="bg-white flex mt-10 overflow-auto py-12 rounded shadow-lg px-4 items-center"
-      >
-        <div v-if="isLoading" class="loader m-0"></div>
-        <div v-for="track in tidalTracks" :key="track.url" class="flex-shrink-0">
-          <Track :track="track" />
-        </div>
-      </div>
+      <TracksList :is-loading="isLoading" :tracks="tracks" />
     </div>
   </div>
 </template>
@@ -21,14 +13,14 @@
 <script>
 const API_URL = "http://192.168.178.55:9000/api";
 import SearchForm from "./components/SearchForm";
-import Track from "./components/Track";
+import TracksList from "./components/TracksList";
 export default {
   components: {
     SearchForm,
-    Track
+    TracksList
   },
   data: () => ({
-    tidalTracks: [],
+    tracks: [],
     isLoading: false
   }),
   methods: {
@@ -44,7 +36,7 @@ export default {
 
       const data = await response.json();
       if (data) {
-        this.tidalTracks = [...data.tracks];
+        this.tracks = [...data.tracks];
         this.isLoading = false;
         console.log(data);
       }
@@ -54,36 +46,6 @@ export default {
 </script>
 
 <style>
-/* Center the loader */
-.loader {
-  width: 50px;
-  height: 50px;
-  margin: -75px 0 0 -75px;
-  border: 8px solid #f3f3f3;
-  border-radius: 50%;
-  border-top: 8px solid #38a169;
-  -webkit-animation: spin 2s linear infinite;
-  animation: spin 2s linear infinite;
-}
-
-@-webkit-keyframes spin {
-  0% {
-    -webkit-transform: rotate(0deg);
-  }
-  100% {
-    -webkit-transform: rotate(360deg);
-  }
-}
-
-@keyframes spin {
-  0% {
-    transform: rotate(0deg);
-  }
-  100% {
-    transform: rotate(360deg);
-  }
-}
-
 /* Add animation to "page content" */
 .animate-bottom {
   position: relative;

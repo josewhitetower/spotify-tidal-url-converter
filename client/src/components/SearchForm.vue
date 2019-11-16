49b88de6690bb275f29afa19edda607e5c1c853c
Jose Torreblanca <josewhitetower@gmail.com>
2019-11-16 16:57:02 +0100

    added max-w*lg class and changed button text

--- client/src/components/SearchForm.vue
+++ client/src/components/SearchForm.vue
@@ -5,10 +5,10 @@
       placeholder="Enter URL..."
       required
       pattern="https://open.spotify.com/track/.*"
-      class="h-10 px-4 rounded-l-lg"
+      class="h-10 px-4 rounded-l-lg max-w-lg w-full"
       v-model="spotifyUrl"
     />
-    <button class="bg-green-600 h-10 px-2 rounded-r-lg hover:bg-green-400 text-white">Convert</button>
+    <button class="bg-green-600 h-10 px-2 rounded-r-lg hover:bg-green-400 text-white">Go!</button>
   </form>
 </template>
 
